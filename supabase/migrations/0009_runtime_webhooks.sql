ea08a96aedc8a68364c0be1e7b7758b7001c48e3
zoey <zoeyzbeezbee@gmail.com>
2026-08-16 01:41:13 -0500

    remove obsolete Supabase migration 0009

--- supabase/migrations/0009_runtime_webhooks.sql
+++
@@ -1,5 +0,0 @@
-alter table runtime_deployments add column if not exists webhook_token_hash text;
-
-create unique index if not exists runtime_deployments_webhook_token_hash_unique
-  on runtime_deployments(webhook_token_hash)
-  where webhook_token_hash is not null;
